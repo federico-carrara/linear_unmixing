dfabf9acc9025e44d7817350552a88fc92b2caaf
federico-carrara <federico1.carrara@mail.polimi.it>
2024-07-15 15:22:12 +0200

    refac: changed the way FP matrix is created to avoid transposing in LS

--- methods/LeastSquares.py
+++ methods/LeastSquares.py
@@ -4,21 +4,21 @@ from tqdm import tqdm
 
 def lstsq_fit_2D(mixed_img, fp_ref_matrix):
     _, n_rows, n_cols = mixed_img.shape
-    n_fps = fp_ref_matrix.shape[0]
+    n_fps = fp_ref_matrix.shape[1]
 
     fp_conc_img = np.zeros((n_fps, n_rows, n_cols))
 
     for r in range(n_rows):
         for c in range(n_cols):
             mixed_pixel = mixed_img[:, r, c]
-            fp_conc, residuals, rank, sing_vals = lstsq(fp_ref_matrix.T, mixed_pixel)
+            fp_conc, residuals, rank, sing_vals = lstsq(a=fp_ref_matrix, b=mixed_pixel)
             fp_conc_img[:, r, c] = fp_conc
 
     return fp_conc_img
 
 def lstsq_fit_3D(mixed_img, fp_ref_matrix):
     _, nz, ny, nx = mixed_img.shape
-    n_fps = fp_ref_matrix.shape[0]
+    n_fps = fp_ref_matrix.shape[1]
 
     fp_conc_img = np.zeros((n_fps, nz, ny, nx))
 
@@ -26,7 +26,7 @@ def lstsq_fit_3D(mixed_img, fp_ref_matrix):
         for y in range(ny):
             for x in range(nx):
                 mixed_voxel = mixed_img[:, z, y, x]
-                fp_conc, residuals, rank, sing_vals = lstsq(fp_ref_matrix.T, mixed_voxel)
+                fp_conc, residuals, rank, sing_vals = lstsq(a=fp_ref_matrix, b=mixed_voxel)
                 fp_conc_img[:, z, y, x] = fp_conc
 
     return fp_conc_img
